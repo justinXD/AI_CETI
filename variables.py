6eb9ecc50d0d484f3a807be5ff694398becb5579
justinXD <isaac_justin@hotmail.com>
2022-03-10 14:41:15 -0600

    primer ejercicio con python para IA

---
+++ variables.py
@@ -0,0 +1,11 @@
+#declaramos variables de tipo string y de tipo numero entero
+mensaje = "Hola mundo!!"
+
+numero1 = 1000
+numero2 =  1022
+#sumamos los numeros
+resultado = numero1 + numero2
+#imprimimos el mensaje con un andalogo de los templates de JS
+print(f'{mensaje}, año {resultado}')
+
+#podemos imprimir usando también solo print(variable)
